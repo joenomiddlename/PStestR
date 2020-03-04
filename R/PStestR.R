4e1a7578b704857d009902432afb5d0d501340eb
joenomiddlename <watsonjoe8@gmail.com>
2020-03-03 18:08:02 -0800

    Case sensitivity issues resolved

---
+++ R/PStestR.R
@@ -0,0 +1,21 @@
+#' @name PStestR
+#' @title PStestR: A package for testing for preferential sampling in
+#'   spatio-temporal data.
+#'
+#' @description The PStestR has two important functions:
+#' \code{\link{PSTestInit}} and \code{\link{PSTestRun}}
+#'
+#' @section PSTestInit function:
+#' The PSTestInit function is a helper function for setting up the
+#'   data in the format neccessary for implementing the test.
+#' @section PSTestRun function:
+#' The PSTestRun function is the main function for implementing the
+#'   test. It takes as input the data created by the helper function
+#'   PSTestInit.
+#'
+#'   For detailed examples about how to use the PStestR, consult the
+#'   vignette (i.e. run vignette('PStestR')), or visit
+#'   \url{https://github.com/joenomiddlename/PStestR}.
+#'
+#' @docType package
+NULL
